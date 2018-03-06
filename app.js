2b05a469425c743bb3e44f80624f059521fb28d7
Dmytro_Holysh <Dmytro_Holysh@epam.com>
2018-03-06 17:29:37 +0200

    Changed Google service authentication to JWT

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // TODO: Register bot on Azure & check it in Skype; works fine in botframework-emulator
 const schedule = require('node-schedule');
 
-let authentication = require("./spreadsheets/authentication");
-let getData = require("./spreadsheets/getData");
-let startSkypeBot = require("./skype/bot").startSkypeBot;
-let updateSkypeBot = require("./skype/bot").updateSkypeBot;
+// var authentication = require("./spreadsheets/authentication");
+var getData = require("./spreadsheets/getData");
+var startSkypeBot = require("./skype/bot").startSkypeBot;
+var updateSkypeBot = require("./skype/bot").updateSkypeBot;
 
-let userNames = [];
-let botStarted = false;
-
-function getDataPostToChat(callback) {
-    authentication.authenticate().then((auth) => {
-        getData(auth, callback);
-    });
-}
+var userNames = [];
+var botStarted = false;
+console.log("BOT STARTED");
 
 function getNextInQueue(rows) {
-    let filteredNames = [];
-    let rowLengths = rows.map( row => row.length);
-    let nextPeople = rows.filter( row => row.length === Math.min.apply(null, rowLengths));
-    if (nextPeople.length >= 2) {
-        filteredNames = [nextPeople[0][0], nextPeople[1][0]];
-    } else if (nextPeople.length === 1) {
-        filteredNames = [nextPeople[0][0], rows[0][0]];
+    var filteredNames = [];
+    var newMessage = "";
+    if (typeof rows === "string") {
+        newMessage = "No data available";
     } else {
-        filteredNames = [rows[0][0], rows[1][0]];
+        var rowLengths = rows.map(function (row) {return row.length;});
+        var nextPeople = rows.filter(function (row) {return row.length === Math.min.apply(null, rowLengths);});
+        if (nextPeople.length >= 2) {
+            filteredNames = [nextPeople[0][0], nextPeople[1][0]];
+        } else if (nextPeople.length === 1) {
+            filteredNames = [nextPeople[0][0], rows[0][0]];
+        } else {
+            filteredNames = [rows[0][0], rows[1][0]];
+        }
+        newMessage = filteredNames[0] + " leading today on standup. \n " + filteredNames[1] + " next in queue.";
     }
-    let newMessage = `${filteredNames[0]} leading today on standup. \n${filteredNames[1]} next in queue.`;
     if (botStarted && userNames[0] !== filteredNames[0]) {
         updateSkypeBot(newMessage);
     } else if (!botStarted && userNames[0] !== filteredNames[0]){
@@ -36,15 +36,17 @@ function getNextInQueue(rows) {
     userNames = filteredNames;
 }
 
-
-// getDataPostToChat(getNextInQueue);
-// setInterval(() => getDataPostToChat(getNextInQueue), 15000);
-
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(1, 5)];
 rule.hour = 10;
 rule.minute = 20;
 
+
+// trigger once to check how it works
+getData(getNextInQueue);
+
 var j = schedule.scheduleJob(rule, function(){
-    getDataPostToChat(getNextInQueue);
+    getData(getNextInQueue);
 });
+
+// startSkypeBot("test");
--- skype/bot.js
+++ skype/bot.js
@@ -1,48 +1,71 @@
-let restify = require('restify');
-let builder = require('botbuilder');
+var restify = require('restify');
+var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
 //=========================================================
 // Bot Setup
 //=========================================================
 // Setup Restify Server
-let server = restify.createServer();
-// Create chat bot
-let connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+console.log("============== TEST =================");
+var server = restify.createServer();
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // // Set to true to include cookies in the requests sent
+    // // to the API (e.g. in case of sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Connection', 'keep-alive');
+
+    next();
 });
-let inMemoryStorage = new builder.MemoryBotStorage();
-let bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
-let savedAddress = undefined;
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+// var inMemoryStorage = new builder.MemoryBotStorage();
+
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
+// bot.set('storage', inMemoryStorage);
+
+var savedAddress = undefined;
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
 
 function startSkypeBot(messageWithUserNames) {
-    server.listen(process.env.port || process.env.PORT || 3978, function () {
-        console.log('%s listening to %s', server.name, server.url);
-    });
-    server.post('/api/messages', connector.listen());
 //Bot on
     bot.on('contactRelationUpdate', function (message) {
         if (message.action === 'add') {
-            let name = message.user ? message.user.name : null;
-            let reply = new builder.Message()
+            var name = message.user ? message.user.name : null;
+            var reply = new builder.Message()
                 .address(message.address)
                 .text("Hello %s... Thanks for adding me.", name || 'there');
             bot.send(reply);
         } else {
-            // delete their data
+            // devare their data
         }
     });
     bot.on('typing', function (message) {
         // User is typing
     });
-    bot.on('deleteUserData', function (message) {
-        // User asked to delete their data
+    bot.on('devareUserData', function (message) {
+        // User asked to devare their data
     });
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-    let usersMessageSent = false;
-    let newMessage = messageWithUserNames;
+    var usersMessageSent = false;
+    var newMessage = messageWithUserNames;
     bot.dialog('/', function (session, args) {
         if (!usersMessageSent) {
             savedAddress = session.message.address;
@@ -51,15 +74,15 @@ function startSkypeBot(messageWithUserNames) {
             // session.endDialog();
             usersMessageSent = true;
             // server.destroy(callback());
+        } else if (session.message.toLowerCase().indexOf("ping") !== -1) {
+            session.send("Ping-Pong :)");
+        } else {
+            return;
         }
     }, true);
 
     bot.on('conversationUpdate', function (message) {
-        if (usersMessageSent) {
-            return;
-        } else {
-            bot.beginDialog(message.address, '/');
-        }
+        bot.beginDialog(message.address, '/');
     });
 }
 
@@ -68,7 +91,7 @@ function updateSkypeBot(newMessage) {
 }
 
 function startProactiveDialog(address, message) {
-    let msg = new builder.Message().address(address);
+    var msg = new builder.Message().address(address);
     msg.text(message);
     msg.textLocale('en-US');
     bot.send(msg);
@@ -76,3 +99,13 @@ function startProactiveDialog(address, message) {
 
 module.exports.startSkypeBot = startSkypeBot;
 module.exports.updateSkypeBot = updateSkypeBot;
+
+// function startBotTest(messageWithUserNames) {
+//     server.listen(3978, function () {
+//         console.log('%s listening to %s', server.name, server.url);
+//     });
+//     server.post('/api/messages', connector.listen());
+//     bot.dialog('/', function (session, args) {
+//         session.send(messageWithUserNames);
+//     }, true);
+// }
